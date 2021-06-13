d7eaa4da10b9b2fda17f3c697e68e4105f80b961
Vikash Patel <vikaspatelp83@gmail.com>
2021-06-13 14:18:17 +0530

    remove missing auth apis

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const { app, config } = require("./startup");
 
 // APIs
-const authApi = require("./apis/auth");
 const urlViewApi = require("./apis/url_view");
 const urlShortnerApi = require("./apis/url_shortner");
 
 // Routing
-app.use("/auth", authApi);
 app.use("/urls", urlViewApi);
 // always keep root url to the bottom of all custom
 // routes otherwise it will map the route to a short url
