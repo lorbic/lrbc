8002e27901983fa6d37d4191207f8c9a38cb1619
Vikash Patel <vikaspatelp83@gmail.com>
2021-07-11 21:04:54 +0530

    Split auth0 setup code from index.js file

--- apis/auth.js
+++ apis/auth.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res) => {
+  console.log(req.oidc.isAuthenticated() ? "AUTH0:::Logged in" : "AUTH0:::Logged out");
+  res.send(JSON.stringify(req.oidc.isAuthenticated() ? "AUTH0:::Logged in" : "AUTH0:::Logged out"));
+});
+
 router.get("/profile", async (req, res) => {
+  console.log(req.oidc.isAuthenticated() ? "AUTH0:::Logged in" : "AUTH0:::Logged out");
   res.send(JSON.stringify(req.oidc.user));
 });
 
--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const { app } = require("./startup");
-
 const { auth, requiresAuth } = require("express-openid-connect");
-
-const authConfig = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: process.env.AUTH0_SECRET,
-  baseURL: process.env.AUTH0_BASEURL,
-  clientID: process.env.AUTH0_CLIENTID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL,
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(authConfig));
 
 // APIs
 const urlViewApi = require("./apis/url_view");
 const urlshorterApi = require("./apis/url_shorter");
-const authApi = require("./apis/auth");
 
-// Routing
-app.use("/user", requiresAuth(), authApi);
-app.use("/urls", requiresAuth(), urlViewApi);
+// secure apis only on production environment
+if (process.env.NODE_ENV == "production") {
+  const { authConfig } = require("./utils/auth");
+  app.use(auth(authConfig));
+
+  const authApi = require("./apis/auth"); // auth0 user data apis
+
+  app.use("/user", requiresAuth(), authApi);
+  app.use("/urls", requiresAuth(), urlViewApi);
+}
+// development environment non-secure apis
+else {
+  app.use("/urls", urlViewApi);
+}
+
 // always keep root url to the bottom of all custom
 // routes otherwise it will map the route to a short url
 app.use("/", urlshorterApi);
