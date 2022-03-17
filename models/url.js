1a96aa0e8e638ef5eb8b8c08ba1ad11632034c0a
Vikash <vikaspatelp83@gmail.com>
2022-03-17 20:36:12 +0530

    Replace shortid with nanoid configure nanoid to remove similar looking characters and set id size to 6.

--- models/url.js
+++ models/url.js
@@ -1,24 +1,31 @@
 const mongoose = require("mongoose");
-const shortId = require("shortid");
+
+const { customAlphabet } = require("nanoid");
+
+const alphabet = "23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz";
+const idSize = 6; //100 days to 1% repeat chance (17/03/2022) 10 id
+
+const nanoid = customAlphabet(alphabet, idSize);
+
 const urlSchema = new mongoose.Schema({
-  longURL: {
-    type: String,
-    required: true,
-  },
-  short: {
-    type: String,
-    required: true,
-    default: shortId.generate,
-  },
-  clicks: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+    longURL: {
+        type: String,
+        required: true,
+    },
+    short: {
+        type: String,
+        required: true,
+        default: nanoid(),
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 });
 
 module.exports = mongoose.model("Url", urlSchema);
