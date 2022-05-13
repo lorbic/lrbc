032f06d39f0a59108f75cdf809add69020cb054d
Vikash <vikaspatelp83@gmail.com>
2022-05-14 02:26:01 +0530

    setup logging

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { app } = require("./startup");
 const { auth, requiresAuth } = require("express-openid-connect");
+const httpLogger = require('./utils/httplogger')
+
 
 // APIs
 const urlView = require("./apis/url_view");
@@ -21,12 +23,38 @@ else {
   app.use("/urls", urlView);
 }
 
+
+// Logger
+app.use(httpLogger);
+
+
 // short urls using /api/v1?u=https://example.com
 app.use("/api", urlApis);
 
 // always keep root url to the bottom of all custom
 // routes otherwise it will map the route to a short url
 app.use("/", urlshorter);
+
+
+app.get('/errorhandler', (req, res, next) => {
+  try {
+    throw new Error('Wowza!')
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use(logErrors)
+app.use(errorHandler)
+
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+function errorHandler (err, req, res, next) {
+  res.status(500).send('Error!')
+}
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
